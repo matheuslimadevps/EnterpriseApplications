0c703d08cf9641d1c3ca8bb68a161f698f2760a2
matheuslimadevps <matheus.lima@pontosys.com>
2026-04-06 15:23:34 -0300

    Adicionado tela básica de home, login e cadastro.
    Adicionado modelo MVC, sem comunicação com as APIs.
    Até o momento:
    Validação básica de campos;
    Botões com navegação para tela correspondente.

---
+++ src/web/EAS.WebApp.MVC/Controllers/IdentityController.cs
@@ -0,0 +1,61 @@
+﻿using EAS.WebApp.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace EAS.WebApp.MVC.Controllers
+{
+    public class IdentityController : Controller
+    {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+           if(!ModelState.IsValid) return View(usuarioRegistro);
+
+            // API - Registro
+
+            if (false) return View(usuarioRegistro);
+
+            //Realizar login na APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            // API - Login
+
+            if (false) return View(usuarioLogin);
+
+            //Realizar login na APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
